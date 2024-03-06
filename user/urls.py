018c0705a6282ab29126c75c8d56bda28ce9b598
Rontim <rongitonga@gmail.com>
2024-03-06 14:39:49 +0300

    Added an endpoint to handle public user profile

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import AddUserInterests
+from .views import AddUserInterests, GetSUserProfile
 
 urlpatterns = [
     path('interests', AddUserInterests.as_view()),
-
+    path('<str:username>', GetSUserProfile.as_view())
 ]
--- user/views.py
+++ user/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django.shortcuts import get_object_or_404
 from blog.views import User
 from topic.models import Topic
@@ -22,3 +24,23 @@ class AddUserInterests(APIView):
         serializer = UserSerializer(instance=user)
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class GetSUserProfile(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_user(self, username):
+        try:
+            user = User.objects.get(username=username)
+
+        except User.DoesNotExist:
+            raise NotFound
+
+        return user
+
+    def get(self, request, username):
+        profile = self.get_user(username)
+
+        serializer = UserSerializer(instance=profile)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
