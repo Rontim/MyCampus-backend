895adc772ae417820d74a78629a271506d0f784d
muceraaa <muceravn@gmail.com>
2024-03-08 17:56:20 +0300

    Doc(Admin): Added registry for users' interactions in theadminpanel

--- interactions/admin.py
+++ interactions/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 from .models import ClubInteraction, UserInteraction
 
 
-admin.site.register(ClubInteraction)
-admin.site.register(UserInteraction)
+
+class ClubInteractionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'club_id')
+    list_filter = ('user', 'club_id')
+    search_fields = ('user__username', 'club_id__name')
+   
+
+admin.site.register(ClubInteraction, ClubInteractionAdmin)
+
+class UserInteractionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'follow_user')
+    list_filter = ('user', 'follow_user')
+    search_fields = ('user__username', 'follow_user__username')
+    
+
+admin.site.register(UserInteraction, UserInteractionAdmin)
