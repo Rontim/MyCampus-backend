6703b8416f3579c9c2c206d8626cf9a26aaabf7c
Rontim <rongitonga@gmail.com>
2024-03-17 15:48:14 +0300

    Fix(API): Added user profile API endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,3 +1,4 @@
+from user.views import GetSUserProfile
 from blog.views import SearchBlogs, TrendingBlogs
 from topic.views import TrendingTopics
 from django.urls import path
@@ -6,4 +7,5 @@ urlpatterns = [
     path('topics/trending', TrendingTopics.as_view()),
     path('blogs/trending', TrendingBlogs.as_view()),
     path('blogs/search', SearchBlogs.as_view()),
+    path('user/<str:username>', GetSUserProfile.as_view()),
 ]
--- user/urls.py
+++ user/urls.py
@@ -3,5 +3,4 @@ from .views import AddUserInterests, GetSUserProfile
 
 urlpatterns = [
     path('interests', AddUserInterests.as_view()),
-    path('<str:username>', GetSUserProfile.as_view())
 ]
