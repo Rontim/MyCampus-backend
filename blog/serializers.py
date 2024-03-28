081025476dd6cd6866e826b0ce5065205ea0be4b
Mirror83 <glenochieng045@gmail.com>
2024-03-28 17:01:52 +0300

    Added excerpt to blog listing response

--- blog/serializers.py
+++ blog/serializers.py
@@ -10,48 +10,80 @@ class BlogCreateRetrieveSerializer(serializers.ModelSerializer):
     topics = TopicSerializer(many=True)
 
     def to_internal_value(self, data):
-        if 'content' in data:
-            data['content'] = json.dumps(data['content'])
+        if "content" in data:
+            data["content"] = json.dumps(data["content"])
         return super().to_internal_value(data)
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
-        if 'content' in representation:
-            representation['content'] = json.loads(representation['content'])
+        if "content" in representation:
+            representation["content"] = json.loads(representation["content"])
 
         return representation
 
     def create(self, validated_data):
-        topics_data = validated_data.pop('topics', [])
+        topics_data = validated_data.pop("topics", [])
 
         blog = Blog.objects.create(**validated_data)
 
         for topic_data in topics_data:
-            topic, _ = Topic.objects.get_or_create(
-                topic_name=topic_data["topic_name"])
+            topic, _ = Topic.objects.get_or_create(topic_name=topic_data["topic_name"])
             blog.topics.add(topic)
         return blog
 
     class Meta:
         model = Blog
-        fields = ['id', 'title', 'slug', 'author_type', 'author_user', 'author_club', 'thumbnail', 'content',
-                  'topics', 'updated_at', 'created_at', 'likes']
-        read_only_fields = ['id', 'updated_at', 'created_at']
+        fields = [
+            "id",
+            "title",
+            "slug",
+            "author_type",
+            "author_user",
+            "author_club",
+            "thumbnail",
+            "content",
+            "topics",
+            "updated_at",
+            "created_at",
+            "likes",
+        ]
+        read_only_fields = ["id", "updated_at", "created_at"]
 
 
 class BlogListingSerializer(serializers.ModelSerializer):
     topics = UserInterestSerializer(many=True, read_only=True)
     author = serializers.SerializerMethodField()
+    excerpt = serializers.CharField(source="content", read_only=True)
 
     def to_representation(self, instance):
-        return super().to_representation(instance)
+        representation = super().to_representation(instance)
+        if "excerpt" in representation:
+            representation["excerpt"] = json.loads(representation["excerpt"])
+            blocks = representation["excerpt"]["blocks"]
+            for block in blocks:
+                if block["type"] == "paragraph":
+                    representation["excerpt"] = block["data"]["text"]
+                    return representation
+
+            representation["excerpt"] = ""
+
+        return representation
 
     class Meta:
         model = Blog
-        fields = ['title', 'slug', 'thumbnail',
-                  'topics', 'author_type', 'author', 'likes', 'created_at']
+        fields = [
+            "title",
+            "slug",
+            "thumbnail",
+            "topics",
+            "author_type",
+            "excerpt",
+            "author",
+            "likes",
+            "created_at",
+        ]
 
     def get_author(self, instance):
-        if instance.author_type == 'user':
+        if instance.author_type == "user":
             return instance.author_user.username
         return instance.author_club.club_name
